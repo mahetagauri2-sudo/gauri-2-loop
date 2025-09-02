7a4cdda171e58137c17617ecd6ea41c31b57bbb1
mahetagauri2-sudo <mahetagauri2@gmail.com>
2025-09-02 21:21:50 +0530

    Add files via upload

---
+++ loop4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    int i, n;
+    clrscr();
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(i=1; i<=n; i++) {
+        printf("%d ", i);
+    }
+    getch();
+}
